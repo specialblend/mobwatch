e08fdbbc85bb080ee631bedafa134acdca5bbee4
CJ <cj@specialblend.dev>
2023-12-16 23:06:14 -0600

    nit

--- lib/engine.ml
+++ lib/engine.ml
@@ -1,9 +1,9 @@
 let secure_add_player_room ~db room_r player_r =
-  match Room.secure_lookup ~db room_r with
+  match Room.secure_lookup room_r ~db with
   | Error e -> Error e
   | Ok None -> Ok None
   | Ok (Some room) ->
-  match Player.secure_lookup ~db player_r with
+  match Player.secure_lookup player_r ~db with
   | Error e -> Error e
   | Ok None -> Ok None
   | Ok (Some player) -> Room.add_player room player ~db
